6ef09466f625265b80fd6274aac786288737d15c
Senku <289134380+senku1300@users.noreply.github.com>
2026-08-29 16:21:35 +0600

    Update iconUrl to new website address

--- WowProvider/build.gradle.kts
+++ WowProvider/build.gradle.kts
@@ -9,6 +9,6 @@ cloudstream {
         "Movie"
     )
     isCrossPlatform = true
-    iconUrl = "https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://www.wow.xxx/&size=128"
+    iconUrl = "https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://www.wowxxx.to/&size=128"
 }
 
