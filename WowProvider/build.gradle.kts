7767141af7e4e3bd996449d1a33a00eeef00cc31
Senku <289134380+senku1300@users.noreply.github.com>
2026-08-30 23:51:14 +0600

    Update build.gradle.kts

--- WowProvider/build.gradle.kts
+++ WowProvider/build.gradle.kts
@@ -8,7 +8,6 @@ cloudstream {
     tvTypes = listOf(
         "Movie"
     )
-    isCrossPlatform = true
+    isCrossPlatform = false
     iconUrl = "https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://www.wowxxx.to/&size=128"
 }
-
