20f8d04146517826090902b795a83f9c1429a03f
Senku <289134380+senku1300@users.noreply.github.com>
2026-08-30 23:57:20 +0600

    Update build.gradle.kts

--- WowProvider/build.gradle.kts
+++ WowProvider/build.gradle.kts
@@ -11,3 +11,10 @@ cloudstream {
     isCrossPlatform = false
     iconUrl = "https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://www.wowxxx.to/&size=128"
 }
+
+dependencies {
+    val appcompatVersion = "1.6.1"
+    compileOnly("androidx.appcompat:appcompat:$appcompatVersion")
+    compileOnly("com.google.android.material:material:1.9.0")
+    compileOnly("androidx.preference:preference-ktx:1.2.0")
+}
