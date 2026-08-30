0a7c2641e14420f763c9b84c4e2186fcfdbdd416
Senku <289134380+senku1300@users.noreply.github.com>
2026-08-31 00:28:21 +0600

    Update build.gradle.kts

--- WowProvider/build.gradle.kts
+++ WowProvider/build.gradle.kts
@@ -5,15 +5,10 @@ cloudstream {
     authors = listOf("Cloudstream Extension")
     description = "Only NSFW Movies Avalibale"
     status = 1
-    tvTypes = listOf("Movie")
-    isCrossPlatform = false
-    requiresResources = false
+    tvTypes = listOf(
+        "Movie"
+    )
+    isCrossPlatform = true
     iconUrl = "https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://www.wowxxx.to/&size=128"
 }
 
-dependencies {
-    val appcompatVersion = "1.6.1"
-    compileOnly("androidx.appcompat:appcompat:$appcompatVersion")
-    compileOnly("com.google.android.material:material:1.9.0")
-    compileOnly("androidx.preference:preference-ktx:1.2.0")
-}
