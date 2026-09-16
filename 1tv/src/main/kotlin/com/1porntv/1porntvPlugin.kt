07ce2e554fa1ff8661f7019e7ec2805212f2c3c0
senkuboy0-cyber <255331092+senkuboy0-cyber@users.noreply.github.com>
2026-09-16 15:12:03 +0600

    Update 1porntvPlugin.kt

--- 1tv/src/main/kotlin/com/1porntv/1porntvPlugin.kt
+++ 1tv/src/main/kotlin/com/1porntv/1porntvPlugin.kt
@@ -1,12 +1,12 @@
-package com.1porntv
+package com.oneporntv
+
 import com.lagradost.cloudstream3.plugins.BasePlugin
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 
-
 @CloudstreamPlugin
-class WowPlugin: BasePlugin() {
+class OnePornTvPlugin: BasePlugin() {
     override fun load() {
         // All providers should be added in this manner. Please don't edit the providers list directly.
-        registerMainAPI(WowProvider())
+        registerMainAPI(OnePornTvProvider())
     }
 }
