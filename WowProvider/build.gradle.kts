868c0ca43a37e3b41d6642fbdcb995da06b82b5e
senkuboy0-cyber <255331092+senkuboy0-cyber@users.noreply.github.com>
2026-09-18 20:46:19 +0600

    Update build.gradle.kts

--- WowProvider/build.gradle.kts
+++ WowProvider/build.gradle.kts
@@ -1,27 +1,13 @@
-plugins {
-    id("com.lagradost.cloudstream3.gradle")
-}
-
-version = 6
-
-android {
-    buildFeatures {
-        buildConfig = true
-    }
-}
-
-dependencies {
-    implementation("com.google.android.material:material:1.14.0")
-}
+version = 5
 
 cloudstream {
     language = "en"
     authors = listOf("Cloudstream Extension")
-    description = "Only NSFW Movies Available"
+    description = "Only NSFW Movies Avalibale"
     status = 1
     tvTypes = listOf(
         "Movie"
     )
-    isCrossPlatform = false
+    isCrossPlatform = true
     iconUrl = "https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://www.wowxxx.to/&size=128"
 }
