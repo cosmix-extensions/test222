c8f833b7d296e58a26349723aa63f41a96580723
senkuboy0-cyber <255331092+senkuboy0-cyber@users.noreply.github.com>
2026-09-18 20:26:53 +0600

    feat(WowProvider): add Cloudflare WebView bypass support

--- WowProvider/build.gradle.kts
+++ WowProvider/build.gradle.kts
@@ -1,14 +1,27 @@
-version = 5
+plugins {
+    id("com.lagradost.cloudstream3.gradle")
+}
+
+version = 6
+
+android {
+    buildFeatures {
+        buildConfig = true
+    }
+}
+
+dependencies {
+    implementation("com.google.android.material:material:1.14.0")
+}
 
 cloudstream {
     language = "en"
     authors = listOf("Cloudstream Extension")
-    description = "Only NSFW Movies Avalibale"
+    description = "Only NSFW Movies Available"
     status = 1
     tvTypes = listOf(
         "Movie"
     )
-    isCrossPlatform = true
+    isCrossPlatform = false
     iconUrl = "https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://www.wowxxx.to/&size=128"
 }
-
