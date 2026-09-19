d2c6828712c07b49f772cf10e0f895a78553b20d
senkuboy0-cyber <255331092+senkuboy0-cyber@users.noreply.github.com>
2026-09-19 20:34:03 +0600

    Update WowProvider.kt

--- WowProvider/src/main/kotlin/com/wow/WowProvider.kt
+++ WowProvider/src/main/kotlin/com/wow/WowProvider.kt
@@ -14,7 +14,13 @@ class WowProvider : MainAPI() {
     override val supportedTypes = setOf(TvType.Others)
 
     // Changed to Android Mobile User-Agent to avoid Cloudflare fingerprint mismatch
-    private val ua = mapOf("User-Agent" to "Mozilla/5.0 (Linux; Android 13; SM-S901B) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36")
+        private val ua = mapOf(
+        "User-Agent" to "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
+        "Accept" to "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
+        "Accept-Language" to "en-US,en;q=0.9",
+        "Connection" to "keep-alive",
+        "Upgrade-Insecure-Requests" to "1"
+    )
 
     override val mainPage = mainPageOf(
         "$mainUrl/latest-updates/" to "Latest Updates",
