94d101e74c2438e651f1d30e281e73649462caf3
senkuboy0-cyber <255331092+senkuboy0-cyber@users.noreply.github.com>
2026-09-20 17:17:18 +0600

    Create build.gradle.kts

---
+++ FullTo/build.gradle.kts
@@ -0,0 +1,13 @@
+version = 2
+
+cloudstream {
+    language = "en"
+    authors = listOf("Cloudstream Extension")
+    description = "Only NSFW Movies Avalibale"
+    status = 1
+    tvTypes = listOf(
+        "Movie"
+    )
+    isCrossPlatform = true
+    iconUrl = "https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://www.fullporn.to/&size=128"
+}
