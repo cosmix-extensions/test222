edb6d5d83e6b7cc763b77c11951a5967b521811d
senkuboy0-cyber <255331092+senkuboy0-cyber@users.noreply.github.com>
2026-09-20 15:37:30 +0600

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,2 @@
-rootProject.name = "NSFW Providers"
-include(":ViralLinksProvider", ":WowProvider", ":WowUncutProvider", ":HamsterProvider", ":onetv")
+RootProject.name = "NSFW Providers"
+include(":ViralLinksProvider", ":WowProvider", ":WowUncutProvider", ":HamsterProvider", ":onetv", ":FreeVideos")
